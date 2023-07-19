1e601096317c9902730589ded2f2db351b275728
Zuzanna Skokowska <zuzanna.skokowska@icloud.com>
2023-07-19 11:04:01 +0200

    init a next.js app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
   reactStrictMode: true,
-}
+  images: {
+    domains: ["upload.wikimedia.org"],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
