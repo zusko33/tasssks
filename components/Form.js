a650b3f1e58fc1959d4db9973eb7c5f40975bc3b
Zuzanna Skokowska <zuzanna.skokowska@icloud.com>
2023-07-21 17:11:32 +0200

    some style

--- components/Form.js
+++ components/Form.js
@@ -1,17 +1,19 @@
 import styled from "styled-components";
 
-const Button = styled.button`
-  position: relative;
-  background-color: white;
-  border-radius: 8px;
-  color: black;
-`;
+// const Button = styled.button`
+//   position: relative;
+//   background-color: white;
+//   border-radius: 8px;
+//   color: black;
+// `;
 
 export default function Form({ onSubmit }) {
   // if there will be default date add a prop to Form component
   return (
-    <form onSubmit={onSubmit}>
-      <label htmlFor="name">Title:</label>
+    <form onSubmit={onSubmit} className="form-control w-full max-w-xs">
+      <label htmlFor="name" className="label">
+        <span className="label-text">Title: </span>
+      </label>
       <input
         id="name"
         name="name"
@@ -19,14 +21,18 @@ export default function Form({ onSubmit }) {
         maxLength="20"
         // defaultValue={defaultValue?.name}
       />
-      <label htmlFor="type">choose type of task:</label>
+      <label htmlFor="type" className="label">
+        <span className="label-text">choose type of task:</span>
+      </label>
       <select name="type" id="type">
         <option value="🧽 🪣 ">🧽 🪣 </option>
 
         <option value="🗂 📌 ">🗂 📌 </option>
         <option value="🩺 💉">🩺 💉</option>
       </select>
-      <label htmlFor="description">description:</label>
+      <label htmlFor="description" className="label">
+        <span className="label-text">description:</span>
+      </label>
       <textarea
         id="description"
         name="description"
@@ -34,10 +40,10 @@ export default function Form({ onSubmit }) {
         rows="10"
         // defaultValue={defaultValue?.description}
       ></textarea>
-      <Button type="submit">
+      <button className="btn btn-neutral">
         {/* {defaultValue ? "Update this task" : "Add this task"} */}
         Add this task
-      </Button>
+      </button>
     </form>
   );
 }
--- components/Layout.js
+++ components/Layout.js
@@ -2,40 +2,50 @@ import TitleBar from "./TitleBar.js";
 import styled from "styled-components";
 import Head from "next/head.js";
 
-const Main = styled.main`
-  display: grid;
-  gap: 0.5rem;
-  margin-top: 5rem;
-  padding: 0.5rem;
-  position: relative;
-  width: 50%; /* Adjust the width as needed */
-  margin-left: 25%;
-  margin-right: 25%;
-`;
+// const Main = styled.main`
+//   display: grid;
+//   gap: 0.5rem;
+//   margin-top: 5rem;
+//   padding: 0.5rem;
+//   position: relative;
+//   width: 50%; /* Adjust the width as needed */
+//   margin-left: 25%;
+//   margin-right: 25%;
+// `;
 
-const Footer = styled.footer`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  width: 100%;
-  height: 5%;
-  background-color: #f1f1f1;
-  padding: 1rem;
-  color: black;
+// const Footer = styled.footer`
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   position: fixed;
+//   bottom: 0;
+//   left: 0;
+//   width: 100%;
+//   height: 5%;
+//   background-color: #f1f1f1;
+//   padding: 1rem;
+//   color: black;
+// `;
+const Div = styled.div`
+  position: absolute;
+  align-self: center;
 `;
-
 export default function Layout({ children }) {
   return (
     <>
-      <Head>
-        <title>TASSSKS</title>
-      </Head>
-      <TitleBar />
-      <Main>{children}</Main>
-      <Footer>Zuzanna Skokowska ®</Footer>
+      <div className="mockup-phone">
+        <div className="camera"></div>
+        <div className="display">
+          <div className="artboard artboard-demo phone-1">
+            <header>
+              <title>TASSSKS</title>
+            </header>
+            {/* <TitleBar /> */}
+            <main>{children}</main>
+            {/* <footer>Zuzanna Skokowska ®</footer> */}
+          </div>
+        </div>
+      </div>
     </>
   );
 }
